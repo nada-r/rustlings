5cd70faa467d1f353de3989ede237b98ea0c6bd0
nada-r <nadarzer@gmail.com>
2025-12-20 23:51:30 +0100

    str3

--- rustlings/exercises/09_strings/strings3.rs
+++ rustlings/exercises/09_strings/strings3.rs
@@ -1,10 +1,11 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
-    input.to_string().push_str(" world!");
+    input.to_string().push_str(" world!")
 }
 
 fn replace_me(input: &str) -> String {
