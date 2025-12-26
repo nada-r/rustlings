a4d5b553f0895a726468e7e974e6f74174324b7a
nada-r <nadarzer@gmail.com>
2025-12-26 19:51:56 +0100

    mod2

--- rustlings/exercises/10_modules/modules2.rs
+++ rustlings/exercises/10_modules/modules2.rs
@@ -5,6 +5,8 @@ mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
+    use self::delicious_snacks::fruit as PEAR;
+    use self::delicious_snacks::veggie as CUCUMBER ;
 
     mod fruits {
         pub const PEAR: &str = "Pear";
