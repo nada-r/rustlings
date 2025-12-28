89549d9e9de79ea8173ed1e006b0d580878191f7
nada-r <nadarzer@gmail.com>
2025-12-28 21:07:46 +0100

    hash3

--- rustlings/exercises/11_hashmaps/hashmaps3.rs
+++ rustlings/exercises/11_hashmaps/hashmaps3.rs
@@ -10,6 +10,7 @@ use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 #[derive(Default)]
+#[derive(Debug)]
 struct TeamScores {
     goals_scored: u8,
     goals_conceded: u8,
@@ -18,6 +19,7 @@ struct TeamScores {
 fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores = HashMap::<&str, TeamScores>::new();
+    println!("{:?}", scores);
 
     for line in results.lines() {
         let mut split_iterator = line.split(',');
@@ -27,10 +29,16 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
 
+        println!("{}", line);
+        
+
         // TODO: Populate the scores table with the extracted details.
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        scores.entry(team_1_name).or_insert(TeamScores{goals_scored: team_1_score,goals_conceded: team_2_score});
+        scores.entry(team_2_name).or_insert(TeamScores{goals_scored: team_2_score,goals_conceded: team_1_score});
+        println!("{:?}", scores);
     }
 
     scores
@@ -38,6 +46,7 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
 
 fn main() {
     // You can optionally experiment here.
+    println!("test");
 }
 
 #[cfg(test)]
